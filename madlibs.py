76ac5389785c80eb7bb91ece1edebc7a5d23fa47
Tejask001 <88230808+Tejask001@users.noreply.github.com>
2021-11-09 23:14:49 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,28 @@
+# making a madlibs game using string formatting
+print("Welcome to the Madlibs Game.\nThis is a madlib about Political Speech")
+print("Rules of the game: \nYou will be provided with categories and you have to enter a word corresponding to that category")
+print("Lets Play !\n")
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+plr_noun1 = input("Plural Noun: ")
+plr_noun2 = input("Plural Noun: ")
+adj3 = input("Adjective: ")
+noun1 = input("Noun: ")
+noun2 = input("Noun: ")
+plr_noun3 = input("Plural Noun: ")
+adj4 = input("Adjective: ")
+male = input("Male Person: ")
+adj5 = input("Adjective: ")
+noun3 = input("Noun: ")
+adj6 = input("Adjective: ")
+noun4 = input("Noun: ")
+plr_noun4 = input("Plural Noun: ")
+plr_noun5 = input("Plural Noun: ")
+adj7 = input("Adjective: ")
+adj8 = input("Adjective: ")
+adj9 = input("Adjective: ")
+
+madlib = f"Ladies and gentlemen, on this {adj1} occasion it is a privilege to adress such a/an {adj2} looking group of {plr_noun1}. I can tell from your smiling {plr_noun2} that you will support my {adj3} program in the coming election. I promise that, if elected, there will be a/an {noun1} in every {noun2} and two {plr_noun3} in every garage. I want to warn you against my {adj4} opponent, Mr.{male}. The man is nothing but a/an {adj5} {noun3}. He has a/an {adj6} character and is working {noun4} in glove with the criminal element. If elected, I promise to eliminate vice. I will keep the {plr_noun4} off the streets. I will keep crooks from dipping their {plr_noun5} in the public till. I promise you {adj7} government, {adj8} taxes, and {adj9} schools."
+
+print("\n\n")
+print(madlib)
